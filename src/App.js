fa114ec55dfc5ad2de73fcf626fae4ab4d903855
leonardo.goes <leonardo.goes@luizalabs.com>
2021-05-14 11:55:16 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Title, Input, Button } from "componentes-lib";
+
+import { Title } from "./components/Title";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Title text="Cadastro de Unidade" color="#ffc900" marginTop={30} />
+      <Input width={300} height={30} name="nome" placeholder="Insira o nome" />
+      <Input
+        width={300}
+        height={30}
+        name="sobrenome"
+        placeholder="Insira o Sobrenome"
+      />
+      <Button marginTop="10px" justifyCenter maxWidth="120px" type="submit">
+        salvar
+      </Button>
+    </React.Fragment>
   );
 }
+
 
 export default App;
